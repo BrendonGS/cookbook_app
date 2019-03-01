93da25fc8190d92cfde32628b12e46410d1200b2
BrendonGS <bgsggermany@gmail.com>
2019-03-01 09:29:59 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {title: "Cookies", chef: "Cookie Monster", ingredients: "cookie mix, milk, eggs, love", directions: "cookie, cookie, cookie, darn, should have bought eggs", image_url: nil, prep_time: 400},
+  {title: "Lagsana", chef: "Garfield", ingredients: "tire, old shoe, peppermint toothpaste", directions: "pile ingredients ", image_url: nil, prep_time: 400},
+  {title: "Lagsana", chef: "Garfield", ingredients: "tire, old shoe, peppermint toothpaste", directions: "pile ingredients ", image_url: nil, prep_time: 400},
+  {title: "butter", chef: "stamp", ingredients: "nuts", directions: "smash", image_url: nil, prep_time: 2},
+  {title: "Coffee", chef: "Joe", ingredients: "brown circles", directions: "crush brown circles", image_url: "https://previews.123rf.com/images/chrescht/chrescht1208/chrescht120800005/15214393-closeup-of-crushed-coffee-beans.jpg", prep_time: 12},
+  {title: "BJ", chef: "Top", ingredients: "Mouth", directions: "Forceful head motions", image_url: "https://www.nil.com/assets/Uploads/NIL-logo-kvadrat.jpg", prep_time: 2},
+  {title: "Fuck boy", chef: "Twelve", ingredients: "bull shit", directions: "stir up", image_url: "https://www.globalgoals.org/12.png", prep_time: 50},
+  {title: "Hand Sandwich", chef: "Josh", ingredients: "left hand, right hand, lettuce, mayo, avocado", directions: "place left palm up, lay other ingredients on top, finish with right, place hands togther, good night, wisconsin!", image_url: "http://i54.tinypic.com/1zbf31c.jpg", prep_time: 15},
+  {title: "Raw Egg", chef: "Bad Josh", ingredients: "live chicken, pair of gloves", directions: "place gloves on hands, grip chickens body, squeeze gently", image_url: "https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/92/32/453023821.jpg", prep_time: 5}
+])
+
